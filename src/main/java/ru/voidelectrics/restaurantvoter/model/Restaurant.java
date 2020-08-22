878af5690b2c31d9c611a24fd848e1b158c40851
Maksim Levin <levmaxmih88@mail.ru>
2020-08-23 00:52:31 +1000

    Connected lombok

--- src/main/java/ru/voidelectrics/restaurantvoter/model/Restaurant.java
+++ src/main/java/ru/voidelectrics/restaurantvoter/model/Restaurant.java
@@ -1,14 +1,22 @@
 package ru.voidelectrics.restaurantvoter.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.util.Objects;
 
 @Entity
 @Table(name = "restaurants")
+@Setter
+@Getter
+@ToString
+@NoArgsConstructor
 public class Restaurant extends AbstractBaseEntity {
     @Column(name = "name", nullable = false)
     @NotNull
@@ -18,36 +26,5 @@ public class Restaurant extends AbstractBaseEntity {
     public Restaurant(String name) {
         super(null);
         this.name = name;
-    }
-
-    public Restaurant() {
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Restaurant that = (Restaurant) o;
-        return Objects.equals(name, that.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name);
-    }
-
-    @Override
-    public String toString() {
-        return "Restaurant{" +
-                "name='" + name + '\'' +
-                '}';
     }
 }
