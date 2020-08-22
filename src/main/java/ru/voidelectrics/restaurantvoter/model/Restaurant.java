fe868c44a90e7ae7784f348cb87288b035d6da79
Maksim Levin <levmaxmih88@mail.ru>
2020-08-22 14:43:09 +1000

    Added Jackson, run Restaurant controller with hardcoded values

--- src/main/java/ru/voidelectrics/restaurantvoter/model/Restaurant.java
+++ src/main/java/ru/voidelectrics/restaurantvoter/model/Restaurant.java
@@ -1,4 +1,43 @@
 package ru.voidelectrics.restaurantvoter.model;
 
-public class Restaurant {
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Restaurant implements Serializable {
+    private String name;
+
+    public Restaurant(String name) {
+        this.name = name;
+    }
+
+    public Restaurant() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Restaurant that = (Restaurant) o;
+        return Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String toString() {
+        return "Restaurant{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 }
--- src/main/java/ru/voidelectrics/restaurantvoter/web/RestaurantController.java
+++ src/main/java/ru/voidelectrics/restaurantvoter/web/RestaurantController.java
@@ -1,12 +1,29 @@
 package ru.voidelectrics.restaurantvoter.web;
 
-import ru.voidelectrics.restaurantvoter.service.RestaurantService;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.voidelectrics.restaurantvoter.model.Restaurant;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/rest/restaurants", produces = MediaType.APPLICATION_JSON_VALUE)
 public class RestaurantController {
 
-    private final RestaurantService restaurantService;
+    public RestaurantController() {
 
-    public RestaurantController(RestaurantService restaurantService) {
-        this.restaurantService = restaurantService;
     }
+
+    @GetMapping
+    public List<Restaurant> getAll() {
+        List<Restaurant> list = new ArrayList<>();
+        list.add(new Restaurant("Continental"));
+        list.add(new Restaurant("Voshod"));
+
+        return list;
+    }
+
 }
